650e2418c5754f5a16d819a7b7f46d63cbd84363
Jaylin0312 <jielinglee01@gmail.com>
2024-11-07 21:50:58 +0000

    chore: touch up intro

--- src/locale/intro.ts
+++ src/locale/intro.ts
@@ -2,8 +2,8 @@ export default {
   INTRO: {
     TITLE: "Hi there! Nice meeting you",
     DESCRIPTION1:
-      "I am Jaylin currently a final-year student at Singapore Management University (SMU), pursuing a major in Information Systems. I am passionate about building creative software that tackles day to day problems!",
+      "I am Jaylin, a final-year Information Systems student at Singapore Management University (SMU). I am passionate about developing innovative software solutions that address real-world, everyday challenges!",
     DESCRIPTION2:
-      "Passing it forward has always been my principle, hence the creation of this blog. Hope my notes help the beginners like me even by a little :))",
+      "Paying it forward has always been my guiding principle, and this blog is a reflection of that. I hope my notes offer even a bit of help to fellow beginners like me :))",
   },
 };
